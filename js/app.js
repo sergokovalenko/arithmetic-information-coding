36c460ca5ebb53fb273030cbcc3834d57f926e5a
Sergey Kovalenko <kovalenkosergey.98@yandex.ru>
2018-10-08 22:19:42 +0400

    fixed errors

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 import * as algorithm from './arithmetic-information-coding.js';
 
 window.onload = () => {
-  const inputString = '';
+  const inputString = 'ЭТОТ_МЕТОД_ЛУЧШЕ_ХАФФМАНА';
 
   algorithm.getResult(inputString);
 };
--- js/arithmetic-information-coding.js
+++ js/arithmetic-information-coding.js
@@ -1,4 +1,15 @@
 export function getResult(value) {
+	console.log(value);
+	const occurrence = countOccurrence(value);
+	console.log(occurrence);
+	const frequency = countFrequency(occurrence, value.length);
+	console.log(frequency);
+	const arr = getSortedArray(frequency);
+	console.log(arr);
+	const intervals = buildIntervals(arr);
+	console.log(intervals);
+	const finalInterval = getWorkingInterval(intervals, value);
+	console.log(finalInterval);
 }
 
 function countOccurrence(val) {
@@ -11,6 +22,8 @@ function countOccurrence(val) {
 			result[val[i]] += 1
 		}
 	}
+
+	return result;
 }
 
 function countFrequency(obj, count) {
@@ -26,7 +39,7 @@ function countFrequency(obj, count) {
 
 function getSortedArray(freqObj) {
 	const result = [];
-	const keys = Object.keys(newObj);
+	const keys = Object.keys(freqObj);
 
 	keys.forEach((key) => {
 		result.push({
@@ -35,32 +48,36 @@ function getSortedArray(freqObj) {
 		});
 	});
 
-	result.sort((a, b) => a.frequency > b.frequency);
+	result.sort((a, b) => a.frequency < b.frequency);
+
+	return result;
 }
 
 function buildIntervals(arr) {
 	const newArr = arr.reduce((acc, el) => {
-		acc.push(Object.assign({}, el));
+		var a = Object.assign({}, el);
+		acc.push(a);
 		return acc;
 	}, []);
+
 	newArr[0].low = 0;
 	newArr[0].high = newArr[0].frequency;
 
 	for (let i = 1; i < newArr.length; i++) {
 		const prevInd = i - 1;
 		newArr[i].low = newArr[prevInd].high;
-		newArr[i].high = newArr[i].frequency + newArr[prevInd].frequency;
+		newArr[i].high = newArr[i].frequency + newArr[prevInd].high;
 	}
 
-	newArr[newArr.length].high = 1;
+	newArr[newArr.length - 1].high = 1;
 
 	return newArr;
 }
 
-function getWorkingInteval(intervals, str) {
+function getWorkingInterval(intervals, str) {
 	const interval = {};
 	const char = intervals.find((el) => {
-		el.char = str[0];
+		return el.char === str[0];
 	});
 
 	interval.low = char.low
@@ -68,11 +85,11 @@ function getWorkingInteval(intervals, str) {
 
 	for (let i = 1; i < str.length; i++) {
 		const char = intervals.find((el) => {
-			el.char = str[i];
+			return el.char === str[i];
 		});
 
-		interval.low = interval.low + (interval.high - interval.low) * el.low;
-		interval.high = interval.low + (interval.high - interval.high) * el.high;
+		interval.low = interval.low + ((interval.high - interval.low) * char.low);
+		interval.high = interval.low + ((interval.high - interval.low) * char.high);
 	}
 
 	return interval;
